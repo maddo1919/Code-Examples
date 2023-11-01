fb4d74554a2a97c9420dceb5f99a0eeab1adf488
maddodoughty <maddodoughty@live.co.uk>
2023-11-01 20:01:25 +0000

    added printf files

--- Common_Core/libft/srcs/ft_strtrim.c
+++ Common_Core/libft/srcs/ft_strtrim.c
@@ -1,37 +1,81 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pnourish <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/13 20:12:24 by pnourish          #+#    #+#             */
+/*   Updated: 2023/05/14 14:15:49 by pnourish         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
-static	int	is_in_set(char c, const char *set)
+static size_t	st_strlen(const char *s);
+
+static char		*st_strchr(const char *s, char c);
+
+static size_t	st_strlcpy(char *restrict dst, const char *restrict src,
+					size_t dstsize);
+
+char	*ft_strtrim(char const *src, char const *set)
 {
-	while (*set)
-		if (c == *set++)
-			return (0);
-	return (1);
-	
+	size_t	srclen;
+	char	*dst;
+	char	*start;
+	char	*end;
+
+	if (!src || !set)
+		return (NULL);
+	srclen = st_strlen(src);
+	start = (char *)src;
+	end = (char *)src + srclen - 1;
+	while (*start && st_strchr(set, *start))
+		start++;
+	while (end > start && st_strchr(set, *end))
+		end--;
+	srclen = end - start + 1;
+	dst = (char *)malloc(srclen + 1);
+	if (dst == NULL)
+		return (NULL);
+	st_strlcpy(dst, start, srclen + 1);
+	return (dst);
 }
 
-char	*ft_strtrim(char const *s1, char const *set)
+static size_t	st_strlen(const char *s)
 {
-	size_t	start;
-	size_t	end;
-	char	*rtn;
+	size_t	i;
 
-	if (!s1)
-		return (NULL);
-	if (!set)
-		return (ft_strdup(s1));
-	start = 0;
-	end = ft_strlen(s1);
-	while (is_in_set(s1[start], set) == 0)
-		start++;
-	if (start == ft_strlen(s1))
-	{
-		if(!(rtn = ft_strdup("")))
-			return (NULL);
-		else
-			return (rtn);
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+static char	*st_strchr(const char *s, char c)
+{
+	while (*s != c && *s != '\0')
+		s++;
+	if (*s == c)
+		return ((char *)s);
+	return (NULL);
+}
+
+static size_t	st_strlcpy(char *restrict dst, const char *restrict src,
+					size_t dstsize)
+{
+	size_t	i;
+
+	i = 0;
+	while (src[i] && i < dstsize - 1 && dstsize != 0)
+	{	
+		dst[i] = src[i];
+		i++;
 	}
-	while (is_in_set(s1[end -1], set) == 0)
-		end--;
-	rtn = ft_substr(s1, start, end - start);
-	return (rtn);
+	if (dstsize != 0)
+		dst[i] = '\0';
+	while (src[i])
+		i++;
+	return (i);
 }
